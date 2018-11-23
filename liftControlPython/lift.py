f21013e056be7df0b1c6c2fcd8308f81dbc81f71
azuse <misakaxindex@gmail.com>
2018-11-24 04:27:25 +0800

    debug

--- liftControlPython/lift.py
+++ liftControlPython/lift.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-def initial():
+class lift:
     ### lift control pin
     pin_1 = 12
     pin_2 = 16
@@ -20,73 +20,74 @@ def initial():
     led_5 = 33
     led_6 = 37
 
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pin_1,GPIO.OUT)
-    GPIO.setup(pin_2,GPIO.OUT)
-    GPIO.setup(pin_3,GPIO.OUT)
-    GPIO.setup(pin_4,GPIO.OUT)
-    GPIO.setup(pin_high1,GPIO.OUT)
-    GPIO.setup(pin_high2,GPIO.OUT)
-    GPIO.setup(pin_high3,GPIO.OUT)
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin_1,GPIO.OUT)
+        GPIO.setup(pin_2,GPIO.OUT)
+        GPIO.setup(pin_3,GPIO.OUT)
+        GPIO.setup(pin_4,GPIO.OUT)
+        GPIO.setup(pin_high1,GPIO.OUT)
+        GPIO.setup(pin_high2,GPIO.OUT)
+        GPIO.setup(pin_high3,GPIO.OUT)
 
-    GPIO.setup(led_1,GPIO.OUT)
-    GPIO.setup(led_2,GPIO.OUT)
-    GPIO.setup(led_3,GPIO.OUT)
-    GPIO.setup(led_4,GPIO.OUT)
-    GPIO.setup(led_5,GPIO.OUT)
-    GPIO.setup(led_6,GPIO.OUT)
+        GPIO.setup(led_1,GPIO.OUT)
+        GPIO.setup(led_2,GPIO.OUT)
+        GPIO.setup(led_3,GPIO.OUT)
+        GPIO.setup(led_4,GPIO.OUT)
+        GPIO.setup(led_5,GPIO.OUT)
+        GPIO.setup(led_6,GPIO.OUT)
 
-    nowLevel = 1
+        nowLevel = 1
 
-    step = 1
+        step = 1
 
-def lightAllDim():
-    GPIO.output(led_1,GPIO.LOW)
-    GPIO.output(led_2,GPIO.LOW)
-    GPIO.output(led_3,GPIO.LOW)
-    GPIO.output(led_5,GPIO.LOW)
-    GPIO.output(led_6,GPIO.LOW)
-    GPIO.output(led_4,GPIO.LOW)
+    def lightAllDim():
+        GPIO.output(led_1,GPIO.LOW)
+        GPIO.output(led_2,GPIO.LOW)
+        GPIO.output(led_3,GPIO.LOW)
+        GPIO.output(led_5,GPIO.LOW)
+        GPIO.output(led_6,GPIO.LOW)
+        GPIO.output(led_4,GPIO.LOW)
 
 
-def light(floor):
-    lightAllDim()
-    if floor == 1:
-        GPIO.output(led_1,GPIO.HIGH)
-    elif floor == 2:
-        GPIO.output(led_2,GPIO.HIGH)
-    elif floor == 3:
-        GPIO.output(led_3,GPIO.HIGH)
-    elif floor == 4:
-        GPIO.output(led_4,GPIO.HIGH)
-    elif floor == 5:
-        GPIO.output(led_5,GPIO.HIGH)
-    elif floor == 6:
-        GPIO.output(led_6,GPIO.HIGH)
-    else:
-        pass
-
-def goto(toLevel):
-    GPIO.output(pin_high1, GPIO.HIGH)
-    GPIO.output(pin_high2, GPIO.HIGH)
-    GPIO.output(pin_high3, GPIO.HIGH)
-    
-    if toLevel == 0 :
-        return 0
-    else:
-        light(toLevel)
-        deltaLevel = toLevel - nowLevel
-        if deltaLevel > 0:
-            GPIO.output(pin_3, GPIO.HIGH)
-            GPIO.output(pin_1, GPIO.LOW)
-            sleep(step)
-            nowLevel = toLevel
+    def light(floor):
+        lightAllDim()
+        if floor == 1:
+            GPIO.output(led_1,GPIO.HIGH)
+        elif floor == 2:
+            GPIO.output(led_2,GPIO.HIGH)
+        elif floor == 3:
+            GPIO.output(led_3,GPIO.HIGH)
+        elif floor == 4:
+            GPIO.output(led_4,GPIO.HIGH)
+        elif floor == 5:
+            GPIO.output(led_5,GPIO.HIGH)
+        elif floor == 6:
+            GPIO.output(led_6,GPIO.HIGH)
         else:
-            GPIO.output(pin_3, GPIO.HIGH)
-            GPIO.output(pin_1, GPIO.HIGH)
-            sleep(step)
-            nowLevel = toLevel
-        
+            pass
 
-    
+    def goto(toLevel):
+        GPIO.output(pin_high1, GPIO.HIGH)
+        GPIO.output(pin_high2, GPIO.HIGH)
+        GPIO.output(pin_high3, GPIO.HIGH)
+        
+        if toLevel == 0 :
+            return 0
+        else:
+            light(toLevel)
+            deltaLevel = toLevel - nowLevel
+            if deltaLevel > 0:
+                GPIO.output(pin_3, GPIO.HIGH)
+                GPIO.output(pin_1, GPIO.LOW)
+                sleep(step)
+                nowLevel = toLevel
+            else:
+                GPIO.output(pin_3, GPIO.HIGH)
+                GPIO.output(pin_1, GPIO.HIGH)
+                sleep(step)
+                nowLevel = toLevel
+            
+
+        
 
--- liftControlPython/testLift.py
+++ liftControlPython/testLift.py
@@ -1,5 +1,6 @@
 import lift
 
+l = lift()
 lift.initial()
 lift.goto(3)
 lift.goto(1)
